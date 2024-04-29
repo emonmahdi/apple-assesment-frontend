d4b9d919730ec7817421b434c4f7151402b01a70
emonmahdi <developeremon1997@gmail.com>
2024-04-29 22:31:27 +0600

    connect tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <>
-      <h2>Apple Products</h2>
+      <h2 className="text-3xl font-bold underline bg-purple-500 p-3">
+        New Apple Products
+      </h2>
     </>
   );
 }
