1ae20d90b03eb9e11606824db87053571cd88707
emonmahdi <developeremon1997@gmail.com>
2024-04-30 12:16:36 +0600

    bannar and hero area [done]

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,6 +9,15 @@ const Header = () => {
           Recycle for free
         </a>
       </div>
+      <div className="banner-area">
+        <div className="hero-content text-center p-3 rounded-md w-[547px] mx-auto">
+          <h2 className="hero-title">Apple 2030</h2>
+          <p className="hero-sub-title">
+            A plan as innovative as our products.
+          </p>
+          <button className="learn-more-btn">Learn More</button>
+        </div>
+      </div>
     </div>
   );
 };
